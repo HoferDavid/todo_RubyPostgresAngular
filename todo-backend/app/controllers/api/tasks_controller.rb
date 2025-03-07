950d3e82306d3d0427c62e3a06b40ab0c9a64c62
David Hofer <chassis.poppets0x@icloud.com>
2025-03-07 11:32:57 -0300

    feat: Restore todo-backend and add files to monorepo

---
+++ todo-backend/app/controllers/api/tasks_controller.rb
@@ -0,0 +1,23 @@
+class Api::TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token # deactivate CSRF protection
+
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :completed)
+  end
+end
